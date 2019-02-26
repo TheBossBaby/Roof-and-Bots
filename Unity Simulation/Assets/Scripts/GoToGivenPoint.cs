532f8d35bf37014a6a14ba421820993986dece38
TheBossBaby <pksimpleid2@gmail.com>
2019-02-26 18:41:49 +0530

    Unity Simulation for automated Routing

---
+++ Unity Simulation/Assets/Scripts/GoToGivenPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToGivenPoint : MonoBehaviour
+{
+    public Transform goal;
+    public Transform intialPosition;
+    private UnityEngine.AI.NavMeshAgent agent;       
+    void Start ()
+    {
+        intialPosition = gameObject.GetComponent<Transform>();
+        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+        if (agent == null)
+        {
+            print("GoToGivenPoint: no agent found.");
+        }
+        agent.destination = goal.position;
+    }
+
+    void Update()
+    {
+        if (goal.position == agent.transform.position)
+        {
+            
+        }        
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.name == "Goal")
+        {
+            Destroy(col.gameObject);
+            agent.destination = intialPosition.position;
+        }
+    }
+}
